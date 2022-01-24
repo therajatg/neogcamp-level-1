bf89649733f6f7e5610bab4887528f265b92d57b
Parul Gupta <dguptarajat@gmail.com>
2022-01-24 11:54:47 +0530

    style
    added image and pointer functionality

--- week-two-assigment/drawer-component/script.js
+++ week-two-assigment/drawer-component/script.js
@@ -5,6 +5,11 @@ function childZeroClicked(){
     child[1].style.backgroundColor = "white"
     child[2].style.backgroundColor = "white"
     child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
 }
 
 function childOneClicked(){
@@ -12,21 +17,99 @@ function childOneClicked(){
     child[1].style.backgroundColor = "lightblue"
     child[2].style.backgroundColor = "white"
     child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
 }
+
 
 function childTwoClicked(){
     child[0].style.backgroundColor = "white"
     child[1].style.backgroundColor = "white"
     child[2].style.backgroundColor = "lightblue"
     child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
 }
+
 
 function childThreeClicked(){
     child[0].style.backgroundColor = "white"
     child[1].style.backgroundColor = "white"
     child[2].style.backgroundColor = "white"
     child[3].style.backgroundColor = "lightblue"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
 }
+
+function childFourClicked(){
+    child[0].style.backgroundColor = "white"
+    child[1].style.backgroundColor = "white"
+    child[2].style.backgroundColor = "white"
+    child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "lightblue"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
+}
+
+function childFiveClicked(){
+    child[0].style.backgroundColor = "white"
+    child[1].style.backgroundColor = "white"
+    child[2].style.backgroundColor = "white"
+    child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "lightblue"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
+}
+
+function childSixClicked(){
+    child[0].style.backgroundColor = "white"
+    child[1].style.backgroundColor = "white"
+    child[2].style.backgroundColor = "white"
+    child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "lightblue"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "white"
+}
+
+function childSevenClicked(){
+    child[0].style.backgroundColor = "white"
+    child[1].style.backgroundColor = "white"
+    child[2].style.backgroundColor = "white"
+    child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "lightblue"
+    child[8].style.backgroundColor = "white"
+}
+
+function childEightClicked(){
+    child[0].style.backgroundColor = "white"
+    child[1].style.backgroundColor = "white"
+    child[2].style.backgroundColor = "white"
+    child[3].style.backgroundColor = "white"
+    child[4].style.backgroundColor = "white"
+    child[5].style.backgroundColor = "white"
+    child[6].style.backgroundColor = "white"
+    child[7].style.backgroundColor = "white"
+    child[8].style.backgroundColor = "lightblue"
+}
+
 
 
 
@@ -34,3 +117,8 @@ child[0].addEventListener("click", childZeroClicked);
 child[1].addEventListener("click", childOneClicked);
 child[2].addEventListener("click", childTwoClicked);
 child[3].addEventListener("click", childThreeClicked);
+child[4].addEventListener("click", childFourClicked);
+child[5].addEventListener("click", childFiveClicked);
+child[6].addEventListener("click", childSixClicked);
+child[7].addEventListener("click", childSevenClicked);
+child[8].addEventListener("click", childEightClicked);
